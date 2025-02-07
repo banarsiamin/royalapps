0ad5f5672e72e5fdd82367bdc2dae7bebf7d5af4
banarsiamin <banarsiamin@gmail.com>
2025-02-07 21:43:47 +0530

    Create home.php

---
+++ home.php
@@ -0,0 +1,12 @@
+this …or create a new repository on the command line
+echo "# royalapps" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/banarsiamin/royalapps.git
+git push -u origin main
+…or push an existing repository from the command line
+git remote add origin https://github.com/banarsiamin/royalapps.git
+git branch -M main
+git push -u origin main
